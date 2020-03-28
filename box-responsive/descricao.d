e7be11b1a8d515da69f53c696a0b14800aed7cf7
Christy Hauschild Schott <62401570+ChristySchott@users.noreply.github.com>
2020-03-28 07:41:41 -0300

    Create descricao.d

---
+++ box-responsive/descricao.d
@@ -0,0 +1,6 @@
+Cardápio responsivo que pode vir a ser utilizado no site de algum restaurante ou estabelecimento relacionado. Código muito para trabalahr com colunas e medias.
+
+Você irá trabalhar com:
+- Medias queries
+- Colunas
+- Flexbox
